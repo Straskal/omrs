c7d88eb8d120e4026287ee04b3c6e5193b46bd91
Stephen Traskal <Straskal@gmail.com>
2019-07-18 23:02:37 -0400

    tile map tools

--- assets/editor/tilemaptools.lua
+++ assets/editor/tilemaptools.lua
@@ -3,11 +3,19 @@ local mouse = require("milk.mouse")
 local gui = require("utils.gui")
 local mousebuttons = mouse.buttons
 
+--=================================================
+-- TILE MAP TOOL STATE
+--[[
+    TODO:
+    - undo, redo for paint tools @matt ames
+--]]
+--=================================================
 local tilemaptools = {
-    display_name = "tile",
-    current_tile = 0,
+    panel = {w = 138, h = 640},
     tile_grid = {},
-    selected_tile_src = {}
+    selected_tile = 0,
+    selected_tile_src = {},
+    tilepicker_offset = {x = 5, y = 15}
 }
 
 function tilemaptools:open(editstate)
@@ -36,17 +44,22 @@ function tilemaptools:tick(editstate)
 end
 -- luacheck: pop
 
-function tilemaptools:draw(editstate)
-    -- panel and title
-    local panelw, panelh = 128 + 10, 640
+-- LEFT PANEL
+--=================================================
+local function draw_left_panel(self)
+    local panelw, panelh = self.panel.w, self.panel.h
     gui:panel(0, 0, panelw, panelh)
     gui:label(5, 5, "Tiles")
+end
 
-    -- draw tile pickers
+--=================================================
+-- TILE PICKER
+--=================================================
+local function draw_tile_picker(self, editstate)
     local tilesheet = editstate.tilesheet
     local cellsz = editstate.grid_cell_size
-    local xoffset = 5
-    local yoffset = 15
+    local xoffset = self.tilepicker_offset.x
+    local yoffset = self.tilepicker_offset.y
     for i = 1, #self.tile_grid do
         for j = 1, #self.tile_grid[i] do
             local src = self.tile_grid[i][j].src
@@ -56,29 +69,66 @@ function tilemaptools:draw(editstate)
         xoffset = 5
         yoffset = yoffset + cellsz
     end
+end
 
-    if mouse.is_button_pressed(mousebuttons.LEFT) then
-        -- handling picking tile
-        local tilex = (math.floor((editstate.mouse_state.x - 5) / editstate.grid_cell_size) + 1)
-        local tiley = (math.floor((editstate.mouse_state.y - 15) / editstate.grid_cell_size) + 1)
-        if self.tile_grid[tiley] and self.tile_grid[tiley][tilex] then
-            self.current_tile = self.tile_grid[tiley][tilex].id
-            self.selected_tile_src.x, self.selected_tile_src.y = ((tilex - 1) * cellsz) + 5, ((tiley - 1) * cellsz) + 15
-        end
-    elseif mouse.is_button_down(mousebuttons.LEFT) and self.current_tile ~= 0 then
-        -- or painting tile
-        local msx, msy = editstate.camera:screen2world(editstate.mouse_state.x, editstate.mouse_state.y)
-        local gridx = math.floor(msx / editstate.grid_cell_size) + 1
-        local gridy = math.floor(msy / editstate.grid_cell_size) + 1
-        local tiles = editstate.level.tilemap.tiles
-        if tiles[gridy] and tiles[gridy][gridx] then
-            tiles[gridy][gridx] = self.current_tile
-        end
+local function try_pick_tile(self, editstate)
+    local xoffset = self.tilepicker_offset.x
+    local yoffset = self.tilepicker_offset.y
+    local tilex = (math.floor((editstate.mouse_state.x - xoffset) / editstate.grid_cell_size) + 1)
+    local tiley = (math.floor((editstate.mouse_state.y - yoffset) / editstate.grid_cell_size) + 1)
+    if self.tile_grid[tiley] and self.tile_grid[tiley][tilex] then
+        self.selected_tile = self.tile_grid[tiley][tilex].id
+        self.selected_tile_src.x, self.selected_tile_src.y =
+            ((tilex - 1) * editstate.grid_cell_size) + xoffset,
+            ((tiley - 1) * editstate.grid_cell_size) + yoffset
     end
-    if self.current_tile ~= 0 then
+end
+
+local function highlight_selected_tile(self, editstate)
+    if self.selected_tile ~= 0 then
         graphics.set_draw_color(1, 1, 1, 1)
-        graphics.draw_rect(self.selected_tile_src.x, self.selected_tile_src.y, cellsz, cellsz)
+        graphics.draw_rect(
+            self.selected_tile_src.x,
+            self.selected_tile_src.y,
+            editstate.grid_cell_size,
+            editstate.grid_cell_size
+        )
     end
+end
+
+--=================================================
+-- PAINTING
+--=================================================
+local function try_paint_tile(self, editstate)
+    local msx, msy = editstate.camera:screen2world(editstate.mouse_state.x, editstate.mouse_state.y)
+    local gridx = math.floor(msx / editstate.grid_cell_size) + 1
+    local gridy = math.floor(msy / editstate.grid_cell_size) + 1
+    local tiles = editstate.level.tilemap.tiles
+    if tiles[gridy] and tiles[gridy][gridx] then
+        tiles[gridy][gridx] = self.selected_tile
+    end
+end
+
+--=================================================
+-- DRAW
+--=================================================
+local function is_mouse_over(mx, my, x, y, w, h)
+    return mx > x and mx <= x + w and my > y and my <= y + h
+end
+
+function tilemaptools:draw(editstate)
+    draw_left_panel(self)
+    draw_tile_picker(self, editstate)
+
+    if is_mouse_over(editstate.mouse_state.x, editstate.mouse_state.y, 0, 0, self.panel.w, self.panel.h) then
+        if mouse.is_button_pressed(mousebuttons.LEFT) then
+            try_pick_tile(self, editstate)
+        end
+    elseif mouse.is_button_down(mousebuttons.LEFT) and self.selected_tile ~= 0 then
+        try_paint_tile(self, editstate)
+    end
+
+    highlight_selected_tile(self, editstate)
 end
 
 return tilemaptools
