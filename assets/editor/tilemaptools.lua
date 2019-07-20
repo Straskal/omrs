d9eeb900016a9a0fb732a6b297b56f692df5f68b
Stephen Traskal <Straskal@gmail.com>
2019-07-20 11:58:58 -0400

    cleaning

--- assets/editor/tilemaptools.lua
+++ assets/editor/tilemaptools.lua
@@ -12,25 +12,30 @@ local mousebuttons = mouse.buttons
 --=================================================
 local tilemaptools = {
     panel = {w = 138, h = 345},
-    tile_grid = {},
-    selected_tile = 0,
-    selected_tile_src = {},
-    tilepicker_offset = {x = 5, y = 15}
+    tile_picker = {
+        grid = {},
+        selected = 0,
+        selected_src = {},
+        offset = {x = 5, y = 15},
+        selected_color = {1, 1, 1, 1}
+    }
 }
 
 function tilemaptools:open(editstate)
-    -- create grid from tile definitions
+    -- create grid from array of tile definitions
     local tiledefs = editstate.tileset.tiledefinitions
-    local rows = math.ceil(#tiledefs / 4)
+    local grid = self.tile_picker.grid
+    -- we want to display the tiles in rows of 4
+    local columns = math.ceil(#tiledefs / 4)
 
     local curr = 1
-    for i = 1, rows do
-        self.tile_grid[i] = {}
+    for i = 1, columns do
+        grid[i] = {}
         for j = 1, 4 do
             if curr > #tiledefs then
                 break
             end
-            self.tile_grid[i][j] = tiledefs[curr]
+            grid[i][j] = tiledefs[curr]
             curr = curr + 1
         end
     end
@@ -56,13 +61,15 @@ end
 -- TILE PICKER
 --=================================================
 local function draw_tile_picker(self, editstate)
+    local grid = self.tile_picker.grid
     local tilesheet = editstate.tilesheet
     local cellsz = editstate.grid.cell_size
-    local xoffset = self.tilepicker_offset.x
-    local yoffset = self.tilepicker_offset.y
-    for i = 1, #self.tile_grid do
-        for j = 1, #self.tile_grid[i] do
-            local src = self.tile_grid[i][j].src
+    local xoffset = self.tile_picker.offset.x
+    local yoffset = self.tile_picker.offset.y
+
+    for i = 1, #grid do
+        for j = 1, #grid[i] do
+            local src = grid[i][j].src
             graphics.drawx(tilesheet, xoffset, yoffset, src.x, src.y, cellsz, cellsz, 1, 1, 0)
             xoffset = xoffset + cellsz
         end
@@ -72,24 +79,27 @@ local function draw_tile_picker(self, editstate)
 end
 
 local function try_pick_tile(self, editstate)
-    local xoffset = self.tilepicker_offset.x
-    local yoffset = self.tilepicker_offset.y
-    local tilex = (math.floor((editstate.mouse_state.x - xoffset) / editstate.grid.cell_size) + 1)
-    local tiley = (math.floor((editstate.mouse_state.y - yoffset) / editstate.grid.cell_size) + 1)
-    if self.tile_grid[tiley] and self.tile_grid[tiley][tilex] then
-        self.selected_tile = self.tile_grid[tiley][tilex].id
-        self.selected_tile_src.x, self.selected_tile_src.y =
-            ((tilex - 1) * editstate.grid.cell_size) + xoffset,
-            ((tiley - 1) * editstate.grid.cell_size) + yoffset
+    local grid = self.tile_picker.grid
+    local xoffset = self.tile_picker.offset.x
+    local yoffset = self.tile_picker.offset.y
+    local cellsz = editstate.grid.cell_size
+    local tilex = (math.floor((editstate.mouse_state.x - xoffset) / cellsz) + 1)
+    local tiley = (math.floor((editstate.mouse_state.y - yoffset) / cellsz) + 1)
+
+    if grid[tiley] and grid[tiley][tilex] then
+        self.tile_picker.selected = grid[tiley][tilex].id
+        self.tile_picker.selected_src.x, self.tile_picker.selected_src.y =
+            ((tilex - 1) * cellsz) + xoffset,
+            ((tiley - 1) * cellsz) + yoffset
     end
 end
 
 local function highlight_selected_tile(self, editstate)
-    if self.selected_tile ~= 0 then
-        graphics.set_draw_color(1, 1, 1, 1)
+    if self.tile_picker.selected ~= 0 then
+        graphics.set_draw_color(table.unpack(self.tile_picker.selected_color))
         graphics.draw_rect(
-            self.selected_tile_src.x,
-            self.selected_tile_src.y,
+            self.tile_picker.selected_src.x,
+            self.tile_picker.selected_src.y,
             editstate.grid.cell_size,
             editstate.grid.cell_size
         )
@@ -104,6 +114,7 @@ local function switch_tile_at_mouse(editstate, newtile)
     local gridx = math.floor(msx / editstate.grid.cell_size) + 1
     local gridy = math.floor(msy / editstate.grid.cell_size) + 1
     local tiles = editstate.level.tilemap.tiles
+
     if tiles[gridy] and tiles[gridy][gridx] and tiles[gridy][gridx] ~= newtile then
         tiles[gridy][gridx] = newtile
     end
@@ -125,9 +136,11 @@ function tilemaptools:draw(editstate)
         if mouse.is_button_pressed(mousebuttons.LEFT) then
             try_pick_tile(self, editstate)
         end
-    elseif mouse.is_button_down(mousebuttons.LEFT) and self.selected_tile ~= 0 then
-        switch_tile_at_mouse(editstate, self.selected_tile)
+    elseif mouse.is_button_down(mousebuttons.LEFT) and self.tile_picker.selected ~= 0 then
+        -- paint
+        switch_tile_at_mouse(editstate, self.tile_picker.selected)
     elseif mouse.is_button_down(mousebuttons.RIGHT) then
+        -- erase
         switch_tile_at_mouse(editstate, 0)
     end
 
