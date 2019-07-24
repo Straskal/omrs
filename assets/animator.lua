e1f66ba9cff8fe97ef4d2836022e6f1c2b89b3df
Stephen Traskal <Straskal@gmail.com>
2019-07-23 22:28:22 -0400

    animation

---
+++ assets/animator.lua
@@ -0,0 +1,43 @@
+local animator = {}
+
+local function new(config)
+    local self = {
+        frame_width = config.frame_width,
+        frame_height = config.frame_height,
+        rows = config.rows,
+        columns = config.columns,
+        seconds_per_frame = config.seconds_per_frame or 0.1,
+        current_anim = config.initial_anim,
+        last_anim_time = 0,
+        current_anim_frame = 1,
+        accumulated_time = 0,
+        time = 0
+    }
+    setmetatable(self, { __index = animator })
+    return self
+end
+
+function animator:set_animation(animation)
+    if self.current_anim ~= animation then
+        self.current_anim = animation
+        self.current_anim_frame = 1
+    end
+end
+
+function animator:update(dt)
+    self.time = self.time + dt
+    if self.time - self.last_anim_time > self.seconds_per_frame then
+        self.current_anim_frame = self.current_anim_frame + 1
+        if self.current_anim_frame > #self.current_anim then
+            self.current_anim_frame = 1
+        end
+        self.last_anim_time = self.time
+    end
+    local row = math.floor((self.current_anim[self.current_anim_frame] - 1) / self.columns)
+    local column = math.floor((self.current_anim[self.current_anim_frame] - 1) % self.columns)
+    local src_x = column * self.frame_width
+    local src_y = row * self.frame_height
+    return src_x, src_y, self.frame_width, self.frame_height
+end
+
+return new
--- assets/gos/player.obj.lua
+++ assets/gos/player.obj.lua
@@ -1,5 +1,6 @@
 local graphics = require("milk.graphics")
 local keyboard = require("milk.keyboard")
+local animator = require("animator")
 local keys = keyboard.keys
 
 local unpack = table.unpack
@@ -10,6 +11,21 @@ return {
     src = {0, 0, 32, 32},
     layer = 1,
     speed = 100,
+    animationclips = {
+        idle = {1, 2, 3, 4, 5, 6}
+    },
+    animator = animator({
+        frame_width = 32,
+        frame_height = 32,
+        rows = 2,
+        columns = 4,
+        seconds_per_frame = 0.1,
+        initial_anim = {1, 2, 3, 4, 5, 6},
+        last_anim_time = 0,
+        current_anim_frame = 1,
+        accumulated_time = 0,
+        time = 0
+    }),
     update = function(self, dt)
         if keyboard.is_key_down(keys.W) then
             self.position[2] = self.position[2] - self.speed * dt
@@ -23,6 +39,7 @@ return {
         if keyboard.is_key_down(keys.D) then
             self.position[1] = self.position[1] + self.speed * dt
         end
+        self.src[1], self.src[2], self.src[3], self.src[4] = self.animator:update(dt)
     end,
     draw = function(self, camera, _)
         local x, y = camera:transform_point(unpack(self.position))
