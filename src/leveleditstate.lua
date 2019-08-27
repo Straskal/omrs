ea054aba76862e8794296ecc961f46743ca54185
Stephen Traskal <Straskal@gmail.com>
2019-08-26 20:46:27 -0400

    Update leveleditstate.lua

--- src/leveleditstate.lua
+++ src/leveleditstate.lua
@@ -401,6 +401,25 @@ end
 function editstate:update(dt)
     handleinput(self, dt)
 
+    local layers = self.levelstate.data.tilemap.layers
+    local selectedlayer = self.map.selected_layer
+
+    for i = 1, #layers do
+        local color = layers[i].color
+
+        if self.map.onion then
+            if i < selectedlayer then
+                color[1], color[2], color[3], color[4] = 0.25, 0.25, 0.25, 1
+            elseif i > selectedlayer then
+                color[1], color[2], color[3], color[4] = 0.4, 0.4, 0.4, 0.07
+            else
+                color[1], color[2], color[3], color[4] = 1, 1, 1, 1
+            end
+        else
+            color[1], color[2], color[3], color[4] = 1, 1, 1, 1
+        end
+    end
+
     -- we don't want to update the scene, but we still need to refresh the go lists.
     self.levelstate:refreshgolists()
 end
